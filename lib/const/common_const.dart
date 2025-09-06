4ad7e59e291f2aa13b379dd7f989e936cdb7054f
Taira <b3256@oic.jp>
2025-09-06 16:44:21 +0900

    定数用のdart作成

---
+++ lib/const/common_const.dart
@@ -0,0 +1,33 @@
+/// アプリ内で共通して使う定数をまとめる
+class CommonConstants {
+  // ---- Routes ----
+
+  /// トップページ
+  static const String routeTop = '/';
+
+  /// ログイン
+  static const String routeLogin = '/login';
+
+  /// 会員登録
+  static const String routeRegister = '/register';
+
+  /// 設定
+  static const String routeSettings = '/settings';
+
+  /// プロジェクト作成
+  static const String routeCreateProject = '/create_project';
+
+  /// プロジェクト
+  static const String routeProject = '/project';
+
+  /// タスク一覧
+  static const String routeTaskList = '/task_list';
+
+  // ---- PathParameters ----
+  /// プロジェクトID
+  static const String pathParameterProjectId = 'projectId';
+
+  // ---- LocalStorage Keys ----
+  /// 参加したプロジェクト
+  static const String storageJoinedProject = 'joined_project';
+}
